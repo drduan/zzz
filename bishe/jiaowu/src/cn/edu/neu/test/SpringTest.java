75c7bd768ea9b216447fab446cf761e67b16e1cb
drduan <drduan@vip.qq.com>
2018-04-30 17:33:00 +0800

    poi

--- bishe/jiaowu/src/cn/edu/neu/test/SpringTest.java
+++ bishe/jiaowu/src/cn/edu/neu/test/SpringTest.java
@@ -1,13 +1,31 @@
 package cn.edu.neu.test;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
 
 public class SpringTest {
-
-	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("config/spring-common.xml");
-		Object userMapper = ctx.getBean("userMapper");
-		System.out.println(userMapper);
+	//支持97-2003
+	  public static final String SAMPLE_XLSX_FILE_PATH = "C:\\Users\\DaShang\\Desktop\\1w.xls";
+	public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException {
+//	/	ApplicationContext ctx = new ClassPathXmlApplicationContext("config/spring-common.xml");
+//		Object userMapper = ctx.getBean("userMapper");
+//		System.out.println(userMapper);
+	
+//		HSSFWorkbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
+		HSSFWorkbook workbook =readFile(SAMPLE_XLSX_FILE_PATH);
+		
+		   System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+	
+	}
+	
+	private static HSSFWorkbook readFile(String filename) throws IOException {
+		try (FileInputStream fis = new FileInputStream(filename)) {
+			return new HSSFWorkbook(fis);        // NOSONAR - should not be closed here
+		}
 	}
 }
